f233ddac2d21dfecdb4d2e0adc6dbde140782920
XYZboom <3242304316@qq.com>
2023-10-27 18:16:04 +0800

    <feature> typealias支持

--- src/test/java/depends/extractor/kotlin/KotlinAliasTest.kt
+++ src/test/java/depends/extractor/kotlin/KotlinAliasTest.kt
@@ -24,4 +24,32 @@ class KotlinAliasTest : KotlinParserTest() {
         assertEquals(DependencyType.INHERIT, relations[0].type)
         assertEquals("ParentAlias0", relations[0].entity.rawName.name)
     }
+
+    @Test
+    fun shouldAliasSuccess1() {
+        val src0 = "./src/test/resources/kotlin-code-examples/alias/alias1/package0/ChildAlias1.kt"
+        val src1 = "./src/test/resources/kotlin-code-examples/alias/alias1/package1/ParentAlias1.kt"
+        val parser = createParser()
+        parser.parse(src0)
+        parser.parse(src1)
+        resolveAllBindings()
+        val relations = entityRepo.getEntity("${myPackageName}1.package0.ChildAlias1").relations
+        assertEquals(1, relations.size)
+        assertEquals(DependencyType.INHERIT, relations[0].type)
+        assertEquals("ParentAlias1", relations[0].entity.rawName.name)
+    }
+
+    @Test
+    fun shouldAliasSuccess2() {
+        val src0 = "./src/test/resources/kotlin-code-examples/alias/alias2/package0/ChildAlias2.kt"
+        val src1 = "./src/test/resources/kotlin-code-examples/alias/alias2/package1/ParentAlias2.kt"
+        val parser = createParser()
+        parser.parse(src0)
+        parser.parse(src1)
+        resolveAllBindings()
+        val relations = entityRepo.getEntity("${myPackageName}2.package0.ChildAlias2").relations
+        assertEquals(1, relations.size)
+        assertEquals(DependencyType.INHERIT, relations[0].type)
+        assertEquals("ParentAlias2", relations[0].entity.rawName.name)
+    }
 }
